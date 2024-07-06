e816b8b59961d4ce74e5ff306df900ce60a3f54f
martin <152182688+nitramlam@users.noreply.github.com>
2024-07-06 23:36:00 +0200

    arcadia db name

--- www/config/db.php
+++ www/config/db.php
@@ -7,7 +7,7 @@ function getDatabaseConnection()
     $pdo = null;
 
     try {
-        $database = 'mysql:host=database:3306;dbname=docker';
+        $database = 'mysql:host=database:3306;dbname=arcadia';
         $pdo = new PDO($database, $DBuser, $DBpass);
     } catch (PDOException $e) {
         echo "Error: Unable to connect to MySQL. Error:\n $e<br>";

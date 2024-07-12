c446cc3560f9b99641f442ebd153f78989073cb8
nitramlam <martinlamalle@gmail.com>
2024-07-12 13:17:29 +0200

    creer services

---
+++ www/dashboardEmploye/dashboardEmploye.php
@@ -0,0 +1,15 @@
+<?php require_once(__DIR__ . '/../includes/header.php'); ?>
+
+
+<head>
+ 
+    <link rel="stylesheet" href="dashboardAdmin.css">
+</head>
+<body>
+    <h1>EMPLOYE</h1>
+
+
+<?php require_once (__DIR__ . '/../includes/footer.php'); ?>
+</body>
+</html>
+

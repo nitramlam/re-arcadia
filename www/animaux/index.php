04a825272eb4c48f828dceca57b75d443f2bd063
nitramlam <martinlamalle@gmail.com>
2024-07-21 18:54:03 +0200

    changement animaux

--- www/animaux/index.php
+++ www/animaux/index.php
@@ -32,7 +32,7 @@ if ($pdo) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nos Animaux</title>
-    <link rel="stylesheet" href="animaux.css"> <!-- Assurez-vous de remplacer par le chemin correct de votre fichier CSS -->
+    <link rel="stylesheet" href="animaux.css"> 
 </head>
 <body>
 

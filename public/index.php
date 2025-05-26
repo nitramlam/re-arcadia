2b63713571e4d38a8b8dc32d508e3be075991585
nitramlam <martinlamalle@gmail.com>
2025-05-26 14:11:45 +0200

    docker compose revu

--- public/index.php
+++ public/index.php
@@ -1,11 +1,3 @@
 <?php
-// Autre traitement ou vérification si nécessaire
-
-// Redirection vers index.php
-header('Location: accueil');
-exit; // Assure que le script s'arrête ici pour éviter toute exécution supplémentaire
-?>
-
-
-
-
+header('Location: /accueil/');
+exit;
